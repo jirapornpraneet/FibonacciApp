edad0057030afb3af8a6fa50cb583145ee591f49
Piesoft <nut.jirapornpraneet@gmail.com>
2023-03-18 16:53:30 +0700

    add code

--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getFibonacci()
     }
-
-
+    
+    func getFibonacci() {
+        var num1 = 0
+        var num2 = 1
+        var sum = 0
+        
+        //Fibonacci (0, 1, 1, 2, 3, 5, 8, 13, …)
+        for _ in 0..<10 {
+            sum = num1 //เกบค่า num1 ไว้กับ sum (sum = 0)
+            num1 = num2 //ใส่ค่าให้ num1 ( num1 = num2) (num1 = 1)
+            num2 = sum + num2 //ใส่ค่าให้ num2 (sum + num2 0+1=1)
+            print(sum)
+        }
+    }
 }
 
